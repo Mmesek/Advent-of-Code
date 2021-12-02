578e51b69ed9985fc83f5dc07a47e271ef78032f
Mmesek <13630781+Mmesek@users.noreply.github.com>
2021-12-02 08:59:52 +0100

    Rename func & Remove unnecessary if

--- AoC/solutions/day_01.py
+++ AoC/solutions/day_01.py
@@ -2,17 +2,18 @@ from AoC.utils import get_input
 
 data = get_input(1, lambda x: int(x))
 
-def change(measurements: list[int]) -> int:
+
+def count_increments(measurements: list[int]) -> int:
+    """Count incrementing measurements from previous one"""
     previous_value = None
     increased = 0
     for value in measurements:
-        if not previous_value:
-            previous_value = value
-        else:
+        if previous_value:
             if previous_value < value:
                 increased += 1
-            previous_value = value
+        previous_value = value
     return increased
 
-print(change(data))
-print(change([sum([i, data[x-1], data[x-2]]) for x, i in enumerate(data[2:], start=2)]))
+
+print(count_increments(data))
+print(count_increments([sum([i, data[x - 1], data[x - 2]]) for x, i in enumerate(data[2:], start=2)]))
