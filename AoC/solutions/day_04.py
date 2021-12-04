ab58e73aa7d8934a8ad20746ca975a465f07e62c
Mmesek <13630781+Mmesek@users.noreply.github.com>
2021-12-04 16:18:02 +0100

    Solution to day 4

---
+++ AoC/solutions/day_04.py
@@ -0,0 +1,50 @@
+from AoC.utils import get_input
+
+puzzle_input = get_input(4, lambda x: [int(n) for n in x.replace(",", " ").split(" ") if n])
+
+
+def prepare_boards() -> tuple[list[int], list[list[int]]]:
+    boards = []
+    board = []
+    for i in puzzle_input:
+        if not i:
+            if board:
+                boards.append(board)
+            board = []
+        if i:
+            board.append(i)
+    boards.append(board)
+    order = boards[0][0]
+    boards = boards[1:]
+    return order, boards
+
+
+def sum_unmarked(board: list[list[int]], marked: list[int]) -> int:
+    unmarked = []
+    for row in board:
+        for n in row:
+            if n not in marked:
+                unmarked.append(n)
+    return sum(unmarked)
+
+
+def find_winning_boards() -> int:
+    discarded_boards = []
+    for x, i in enumerate(order):
+        previous_numbers = order[:x] + [i]
+        for _x, board in enumerate(boards):
+            if _x in discarded_boards:
+                continue
+            for column, row in enumerate(board):
+                if all(n in previous_numbers for n in row) or all(
+                    n in previous_numbers for n in [_r[column] for _r in board]
+                ):
+                    u = sum_unmarked(board, previous_numbers)
+                    print(u, i)
+                    yield u * i
+                    discarded_boards.append(_x)
+                    break
+
+
+order, boards = prepare_boards()
+print([score for score in find_winning_boards()])
