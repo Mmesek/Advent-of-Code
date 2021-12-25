813102cd6b64d1f88df06868d71131efe4eb4990
Mmesek <13630781+Mmesek@users.noreply.github.com>
2021-12-25 15:11:10 +0100

    Add typehints & documentation

--- AoC/solutions/day_25.py
+++ AoC/solutions/day_25.py
@@ -1,31 +1,36 @@
 from AoC.utils import get_input
+from copy import deepcopy
 
 
-def move(current_map):
-    from copy import deepcopy
-
+def move(current_map: list[list[str]]) -> list[list[str]]:
+    """If possible, moves cucumbers on matrix"""
     after_horizontal = deepcopy(current_map)
     for y, row in enumerate(current_map):
         for x, cucumber in enumerate(row):
             if cucumber == ">" and row[(x + 1) % len(row)] == ".":
                 after_horizontal[y][(x + 1) % len(row)] = ">"
                 after_horizontal[y][x] = "."
+
     after_vertical = deepcopy(after_horizontal)
     for y, row in enumerate(after_horizontal):
         for x, cucumber in enumerate(row):
             if cucumber == "v" and after_horizontal[(y + 1) % len(current_map)][x] == ".":
                 after_vertical[(y + 1) % len(current_map)][x] = "v"
                 after_vertical[y][x] = "."
+
     return after_vertical
 
 
-def find_step(cucumbers_map):
+def find_step(cucumbers_map: list[list[str]]) -> int:
+    """Moves as long as matrix changes"""
     i = 0
     while True:
         i += 1
         after_move = move(cucumbers_map)
+
         if cucumbers_map == after_move:
             return i
+
         cucumbers_map = after_move
 
 
