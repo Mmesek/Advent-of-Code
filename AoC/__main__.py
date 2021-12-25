bbb40d1da1c96fb2dbaced447e8ac91ed7be0c8d
Mmesek <13630781+Mmesek@users.noreply.github.com>
2021-12-25 14:19:46 +0100

    Add timezone support to switch day on new AoC

--- AoC/__main__.py
+++ AoC/__main__.py
@@ -1,9 +1,10 @@
 import argparse
 import importlib
 from datetime import datetime
+from pytz import timezone
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-day", help="Day's solution to run", default=datetime.now().day, type=int, choices=range(1,26))
+parser.add_argument("-day", help="Day's solution to run", default=datetime.now(tz=timezone("US/Eastern")).day, type=int, choices=range(1,26))
 args = parser.parse_args()
 
 importlib.import_module(f"AoC.solutions.day_{'0'+str(args.day) if args.day < 10 else args.day}", ".")
