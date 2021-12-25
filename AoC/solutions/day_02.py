fa3eab4eb02fc054f5fa94005bd81cf9d157f0a3
Mmesek <13630781+Mmesek@users.noreply.github.com>
2021-12-25 14:19:17 +0100

    Remove unnecesary loop/merge parts

--- AoC/solutions/day_02.py
+++ AoC/solutions/day_02.py
@@ -8,20 +8,15 @@ def answer(horizontal: int, depth: int):
 
 directions = [(d, int(v)) for d, v in get_input(2, lambda x: x.split(" "))]
 
-c = Counter()
-for direction, value in directions:
-    c[direction] += value
-
-answer(c["forward"], abs(c["up"] - c["down"]))
-
 c = Counter({"aim": 0})
 for direction, value in directions:
+    c[direction] += value
     if direction == "forward":
-        c["forward"] += value
         c["depth"] += value * abs(c["aim"])
     elif direction == "down":
         c["aim"] += value
     elif direction == "up":
         c["aim"] -= value
 
+answer(c["forward"], abs(c["up"] - c["down"]))
 answer(c["forward"], c["depth"])
