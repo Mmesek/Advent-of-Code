8a0905be0703799ba030d879160dd75f6697986d
Mmesek <13630781+Mmesek@users.noreply.github.com>
2021-12-25 20:38:06 +0100

    Fix usage without specifying any arguments

--- AoC/__main__.py
+++ AoC/__main__.py
@@ -8,9 +8,9 @@ parser.add_argument("--day", help="Day's solution to run", default=datetime.now(
 parser.add_argument("--year", help="Year's solution to run", default=datetime.now(tz=timezone("US/Eastern")).year, type=str)
 args = parser.parse_args()
 _part = ""
-if len(args.day) > 6:
+if len(str(args.day)) > 6:
     _part = args.day[6:]
     args.day = args.day[:6]
-args.day = int(args.day.strip("day_"))
-args.year = int(args.year.replace("/",".").replace("\\",".").split(".")[-1])
+args.day = int(str(args.day).strip("day_"))
+args.year = int(str(args.year).replace("/",".").replace("\\",".").split(".")[-1])
 importlib.import_module(f"solutions.{args.year}.day_{'0'+str(args.day) if args.day < 10 else args.day}" + _part, ".")
