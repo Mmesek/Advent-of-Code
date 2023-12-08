823cb7228373e0aeee354e138a3eed97c68ba49c
Mmesek <13630781+Mmesek@users.noreply.github.com>
2023-12-08 14:49:05 +0100

    Add solution to Day 7 Part 2 of 2023
    Signed-off-by: Mmesek <13630781+Mmesek@users.noreply.github.com>

--- solutions/2023/day_07.py
+++ solutions/2023/day_07.py
@@ -9,6 +9,7 @@ KTJJT 220
 QQQJA 483""".splitlines()
 
 CARDS = [i.strip() for i in "A, K, Q, J, T, 9, 8, 7, 6, 5, 4, 3, 2".split(",")]
+CARDS_2 = [i.strip() for i in "A, K, Q, T, 9, 8, 7, 6, 5, 4, 3, 2, J".split(",")]
 
 TYPES = {
     7: (5,),
@@ -35,13 +36,42 @@ def compare(a, b) -> int:
     return 0
 
 
-def solution(puzzle_input: list[tuple[str, str]]) -> int:
+def check_wildcards(_hand: tuple[int], cards: tuple[int], wildcards: int):
+    if wildcards == 5:
+        return True
+
+    for x in range(len(_hand)):
+        replaced = [i for i in _hand]
+        replaced[x] += wildcards
+        if tuple(replaced) == cards:
+            return True
+
+    replaced = [i for i in _hand]
+    _wildcards = wildcards
+
+    for x in range(len(_hand)):
+        if _wildcards:
+            replaced[x] += 1
+            _wildcards -= 1
+        if tuple(replaced) == cards:
+            return True
+
+
+def solution(puzzle_input: list[tuple[str, str]], wildcards: bool = False) -> int:
     bids = {hand: int(bid) for hand, bid in puzzle_input}
     hands = []
+    if wildcards:
+        global CARDS
+        CARDS = CARDS_2
 
     for hand, _ in puzzle_input:
-        _hand = tuple(sorted(Counter(hand).values(), reverse=True))
+        _hand = tuple(sorted(Counter(hand.replace("J", "") if wildcards else hand).values(), reverse=True))
         for type, cards in TYPES.items():
+            if wildcards and (_wildcards := hand.count("J")) > 0:
+                if check_wildcards(_hand, cards, _wildcards):
+                    hands.append((hand, type))
+                    break
+
             if _hand == cards:
                 hands.append((hand, type))
                 break
@@ -51,4 +81,5 @@ def solution(puzzle_input: list[tuple[str, str]]) -> int:
     return sum([bids[i[0]] * x for x, i in enumerate(hands, 1)])
 
 
-solve(7, 1, solution, lambda x: x.split(" "), example, 6440)
+solve(7, 1, lambda x: solution(x), lambda x: x.split(" "), example, 6440)
+solve(7, 2, lambda x: solution(x, True), lambda x: x.split(" "), example, 5905)
